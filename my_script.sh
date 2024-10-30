b7331f53607def1a1711af882ae67596582989bd
mr404 <anktbrnamh22@gmail.com>
2024-10-30 05:14:50 +0330

    اولین کامیت

---
+++ my_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# معرفی‌نامه عکسی
+echo "Welcome to My Script!"
+figlet "My Script"
+echo "This script does awesome things."
+echo "======================================"
+
+# بقیه کدهای شما
